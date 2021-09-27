4a2c20ec82e39ddb257f236f2ae4afd2cfcbad56
Tobetch <takutobe@gmail.com>
2021-09-27 15:46:46 +0900

    fix java version to 1.8

---
+++ maven-jar-sample/src/main/java/com/yhayashi30/sample/MyServlet.java
@@ -0,0 +1,17 @@
+package com.yhayashi30.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.*;
+
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>Hello, Servlet World!</body></html>");
+        out.flush();
+        out.close();
+    }
+}
